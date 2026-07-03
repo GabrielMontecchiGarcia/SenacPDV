b2cde899cddbf62e8e305e489e85cbc1baac2e4d
Jose <roniele.bio@gmail.com>
2026-07-03 16:51:13 -0400

    Enuns feitas

---
+++ Models/Enuns/UserType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SenacPDV.Models.Enuns
+{
+    public enum UserType
+    {
+        Admin = 1,
+        User = 2
+    }
+}
