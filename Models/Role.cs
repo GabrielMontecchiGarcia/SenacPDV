19379049ff0cf7ee01e40d63dfd516a1e0030ef1
Jose <roniele.bio@gmail.com>
2026-07-03 15:51:04 -0400

    (Pedro) Criei as models iniciais

---
+++ Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SenacPDV.Models
+{
+    public class Role
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(1), MaxLength(50)]
+        public string Name { get; set; }
+
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ Models/Session.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SenacPDV.Models
+{
+    public class Session
+    {
+        public static User LoggedUser { get; set; }
+    }
+}
