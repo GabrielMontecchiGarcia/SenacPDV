88b773ac2ffbd13846bfcc85ad63b604783c1625
2024.4.14 <2024.4.14@DESKTOP-NGQKHA6>
2026-07-03 16:44:26 -0400

    FormLogin Semi configurado

--- Forms/FormLogin.Designer.cs
+++ Forms/FormLogin.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Login";
+            label1 = new Label();
+            label2 = new Label();
+            TxtUser = new TextBox();
+            TxtPassword = new TextBox();
+            BtnLogin = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Usuário:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(20, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Senha:";
+            // 
+            // TxtUser
+            // 
+            TxtUser.Location = new Point(68, 6);
+            TxtUser.Name = "TxtUser";
+            TxtUser.Size = new Size(155, 23);
+            TxtUser.TabIndex = 2;
+            // 
+            // TxtPassword
+            // 
+            TxtPassword.Location = new Point(68, 40);
+            TxtPassword.Name = "TxtPassword";
+            TxtPassword.Size = new Size(155, 23);
+            TxtPassword.TabIndex = 3;
+            // 
+            // BtnLogin
+            // 
+            BtnLogin.Location = new Point(92, 69);
+            BtnLogin.Name = "BtnLogin";
+            BtnLogin.Size = new Size(75, 23);
+            BtnLogin.TabIndex = 4;
+            BtnLogin.Text = "Login";
+            BtnLogin.UseVisualStyleBackColor = true;
+            BtnLogin.Click += BtnLogin_Click;
+            // 
+            // FormLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(292, 124);
+            Controls.Add(BtnLogin);
+            Controls.Add(TxtPassword);
+            Controls.Add(TxtUser);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FormLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox TxtUser;
+        private TextBox TxtPassword;
+        private Button BtnLogin;
     }
 }
