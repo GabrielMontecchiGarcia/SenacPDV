b7c254dee77284191fd0d6e6afcda9bb580efac6
2024.4.14 <2024.4.14@DESKTOP-PHMQ9A5>
2026-07-07 17:44:00 -0400

    criação do FormsGerente e FormsEstoque

--- Forms/FormAdmin.Designer.cs
+++ Forms/FormAdmin.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormAdmin";
+            menuStrip1 = new MenuStrip();
+            estoqueToolStripMenuItem = new ToolStripMenuItem();
+            estoqueToolStripMenuItem1 = new ToolStripMenuItem();
+            comprasToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { estoqueToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // estoqueToolStripMenuItem
+            // 
+            estoqueToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { estoqueToolStripMenuItem1, comprasToolStripMenuItem });
+            estoqueToolStripMenuItem.Name = "estoqueToolStripMenuItem";
+            estoqueToolStripMenuItem.Size = new Size(60, 20);
+            estoqueToolStripMenuItem.Text = "Gerente";
+            // 
+            // estoqueToolStripMenuItem1
+            // 
+            estoqueToolStripMenuItem1.Name = "estoqueToolStripMenuItem1";
+            estoqueToolStripMenuItem1.Size = new Size(180, 22);
+            estoqueToolStripMenuItem1.Text = "Estoque";
+            estoqueToolStripMenuItem1.Click += estoqueToolStripMenuItem1_Click;
+            // 
+            // comprasToolStripMenuItem
+            // 
+            comprasToolStripMenuItem.Name = "comprasToolStripMenuItem";
+            comprasToolStripMenuItem.Size = new Size(180, 22);
+            comprasToolStripMenuItem.Text = "Compras";
+            // 
+            // FormAdmin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "FormAdmin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FormAdmin";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem estoqueToolStripMenuItem;
+        private ToolStripMenuItem estoqueToolStripMenuItem1;
+        private ToolStripMenuItem comprasToolStripMenuItem;
     }
 }
---
+++ Forms/FormEstoque.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SenacPDV.Forms
+{
+    partial class FormEstoque
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            btnAdicionar = new Button();
+            btnRemover = new Button();
+            btnAtualizar = new Button();
+            txtCodigo = new TextBox();
+            txtNome = new TextBox();
+            txtPreco = new TextBox();
+            lblCodigo = new Label();
+            lblNome = new Label();
+            lblPreco = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(272, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(307, 202);
+            dataGridView1.TabIndex = 0;
+            // 
+            // btnAdicionar
+            // 
+            btnAdicionar.Location = new Point(272, 247);
+            btnAdicionar.Name = "btnAdicionar";
+            btnAdicionar.Size = new Size(75, 23);
+            btnAdicionar.TabIndex = 1;
+            btnAdicionar.Text = "Adicionar";
+            btnAdicionar.UseVisualStyleBackColor = true;
+            btnAdicionar.Click += btnAdicionar_Click;
+            // 
+            // btnRemover
+            // 
+            btnRemover.Location = new Point(374, 247);
+            btnRemover.Name = "btnRemover";
+            btnRemover.Size = new Size(75, 23);
+            btnRemover.TabIndex = 2;
+            btnRemover.Text = "Remover";
+            btnRemover.UseVisualStyleBackColor = true;
+            // 
+            // btnAtualizar
+            // 
+            btnAtualizar.Location = new Point(478, 247);
+            btnAtualizar.Name = "btnAtualizar";
+            btnAtualizar.Size = new Size(75, 23);
+            btnAtualizar.TabIndex = 3;
+            btnAtualizar.Text = "Atualizar";
+            btnAtualizar.UseVisualStyleBackColor = true;
+            // 
+            // txtCodigo
+            // 
+            txtCodigo.Location = new Point(70, 37);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.Size = new Size(100, 23);
+            txtCodigo.TabIndex = 5;
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(70, 82);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(100, 23);
+            txtNome.TabIndex = 6;
+            // 
+            // txtPreco
+            // 
+            txtPreco.Location = new Point(70, 132);
+            txtPreco.Name = "txtPreco";
+            txtPreco.Size = new Size(117, 23);
+            txtPreco.TabIndex = 7;
+            // 
+            // lblCodigo
+            // 
+            lblCodigo.AutoSize = true;
+            lblCodigo.Location = new Point(20, 40);
+            lblCodigo.Name = "lblCodigo";
+            lblCodigo.Size = new Size(46, 15);
+            lblCodigo.TabIndex = 9;
+            lblCodigo.Text = "Código";
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(26, 85);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(40, 15);
+            lblNome.TabIndex = 10;
+            lblNome.Text = "Nome";
+            // 
+            // lblPreco
+            // 
+            lblPreco.AutoSize = true;
+            lblPreco.Location = new Point(26, 135);
+            lblPreco.Name = "lblPreco";
+            lblPreco.Size = new Size(37, 15);
+            lblPreco.TabIndex = 11;
+            lblPreco.Text = "Preço";
+            // 
+            // FormEstoque
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(619, 324);
+            Controls.Add(lblPreco);
+            Controls.Add(lblNome);
+            Controls.Add(lblCodigo);
+            Controls.Add(txtPreco);
+            Controls.Add(txtNome);
+            Controls.Add(txtCodigo);
+            Controls.Add(btnAtualizar);
+            Controls.Add(btnRemover);
+            Controls.Add(btnAdicionar);
+            Controls.Add(dataGridView1);
+            Name = "FormEstoque";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FormEstoque";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button btnAdicionar;
+        private Button btnRemover;
+        private Button btnAtualizar;
+        private TextBox txtCodigo;
+        private TextBox txtNome;
+        private TextBox txtPreco;
+        private Label lblCodigo;
+        private Label lblNome;
+        private Label lblPreco;
+    }
+}
